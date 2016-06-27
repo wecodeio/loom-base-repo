be41ecaeb1233b2276db156132777a3c1e01d6b5
Lucas Videla <videla.lucas@gmail.com>
2016-06-27 20:35:01 -0300

    Blah2

--- src/test/java/edu/tallerweb/conjuntos/ConjuntoTests.java
+++ src/test/java/edu/tallerweb/conjuntos/ConjuntoTests.java
@@ -6,8 +6,18 @@ import org.junit.Test;
 public class ConjuntoTests {
 
 	@Test
-	public void queSeVerificaEjemplo() {
+	public void unoQueDaBien() {
 		Assert.assertTrue(true);
+	}
+
+	@Test
+	public void unoQueDaFailure() {
+		Assert.assertTrue("opa, un failure! atento!", false);
+	}
+
+	@Test
+	public void unoQueDaError() {
+		throw new Exception("Pucha che!");
 	}
 	
 }
